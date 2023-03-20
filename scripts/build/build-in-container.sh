5d1c98e9df42c8cf060fb1f0af95c0ee309c689a
xzchaoo <xzchaoo@gmail.com>
2023-03-20 11:33:28 +0800

    build: supports GOPROXY when building (#18)

--- scripts/build/build-in-container.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# doc: Run this script in docker to build agent binaries.
-
-cd /workspace
-scripts/build/build-using-go.sh
--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -9,8 +9,17 @@ if [ -z "$tag" ]; then
   tag=latest
 fi
 
+# If user defines GOPROXY env, then pass it to docker build using --build-arg.
+build_opts=""
+source ./scripts/build/detect-goproxy.sh
+build_opts=""
+if [ -n "$GOPROXY" ]; then
+  build_opts="--build-arg GOPROXY=$GOPROXY"
+fi
+
 docker buildx build \
   --platform linux/amd64 \
+  $build_opts \
   -f ./scripts/docker/Dockerfile \
   --load \
   -t holoinsight/agent:$tag .
