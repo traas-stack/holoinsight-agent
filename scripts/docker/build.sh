2c8766d58c81c43b659ec389d890e8f5814f1554
xzchaoo <xzchaoo@gmail.com>
2023-04-10 16:49:02 +0800

    build: supports building agent bin using local golang

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -9,17 +9,25 @@ if [ -z "$tag" ]; then
   tag=latest
 fi
 
-# If user defines GOPROXY env, then pass it to docker build using --build-arg.
-build_opts=""
-source ./scripts/build/detect-goproxy.sh
-build_opts=""
-if [ -n "$GOPROXY" ]; then
-  build_opts="--build-arg GOPROXY=$GOPROXY"
+dockerfile=./scripts/docker/Dockerfile
+
+if [ "$local"x = "1"x ]; then
+  # local build is quick but not recommended, because it may have dependencies on local env
+  ./scripts/build/build-using-go.sh
+  tag=local
+  dockerfile=${dockerfile}-local
+else
+  # If user defines GOPROXY env, then pass it to docker build using --build-arg.
+  build_opts=""
+  source ./scripts/build/detect-goproxy.sh
+  build_opts=""
+  if [ -n "$GOPROXY" ]; then
+    build_opts="--build-arg GOPROXY=$GOPROXY"
+  fi
 fi
 
 docker buildx build \
-  --platform linux/amd64 \
-  $build_opts \
-  -f ./scripts/docker/Dockerfile \
-  --load \
-  -t holoinsight/agent:$tag .
+    $build_opts \
+    -f $dockerfile \
+    --load \
+    -t holoinsight/agent:$tag .
