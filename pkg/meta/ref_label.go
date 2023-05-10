4ef03e3fce3112fb0cb99e541804a310a91317be
xzchaoo <xzchaoo@gmail.com>
2023-05-10 22:52:48 +0800

    refactor: refLabels does not overwrite existing keys

--- pkg/meta/ref_label.go
+++ pkg/meta/ref_label.go
@@ -17,6 +17,10 @@ func RefLabels(items []*appconfig.DataConfig_Metric_RefLabels_Item, labels map[s
 
 // RefLabel ref labels as tags
 func RefLabel(item *appconfig.DataConfig_Metric_RefLabels_Item, labels map[string]string, to map[string]string) bool {
+	if _, exist := to[item.Key]; exist {
+		return false
+	}
+
 	var labelValue string
 	for _, label := range item.Labels {
 		labelValue = labels[label]
