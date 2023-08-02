3c24892239c1814b946f5b31bb441cbbd94af560
xzchaoo <xzchaoo@gmail.com>
2023-08-02 17:40:46 +0800

    fix: fix supervisord oom_adj

--- scripts/docker/ensure_supervisord.sh
+++ scripts/docker/ensure_supervisord.sh
@@ -4,4 +4,9 @@ set -e
 if ! ps aux | grep /usr/bin/supervisord | grep -v grep >/dev/null 2>&1; then
   # Run as root with current envs visible
   sudo -E supervisord
+  sleep 1
+  pid_file=/var/run/supervisord.pid
+  pid=`cat $pid_file`
+  # see https://linux.die.net/man/5/proc
+  sudo bash -c "echo -17 > /proc/$pid/oom_adj"
 fi
